f33ab3d0244881cd39ecbd964433c37b3f2d08d6
Kushima <myurin0819@gmail.com>
2022-07-29 10:56:23 +0900

    ホーム画面-つぶやき一覧の情報を配列化

--- Views/home.php
+++ Views/home.php
@@ -5,6 +5,35 @@ ini_set('display_errors', 1);
 date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
 define('HOME_URL', '/TwitterClone/');
+
+////////////////////////////////////////
+// ツイート一覧
+////////////////////////////////////////
+$view_tweets = [
+    [
+        'user_id' => 1,
+        'user_name' => 'taro',
+        'user_nickname' => '太郎',
+        'user_image_name' => 'sample-person.jpg',
+        'tweet_body' => '今プログラミングをしています。',
+        'tweet_image_name' => null,
+        'tweet_created_at' => '2021-03-15 14:00:00',
+        'like-id' => null,
+        'like_count' => 0,
+    ],
+    [
+        'user_id' => 2,
+        'user_name' => 'jiro',
+        'user_nickname' => '次郎',
+        'user_image_name' => null,
+        'tweet_body' => 'コワーキングスペースをオープンしました！',
+        'tweet_image_name' => 'sample-post.jpg',
+        'tweet_created_at' => '2021-03-14 14:00:00',
+        'like-id' => 1,
+        'like_count' => 1,
+    ]
+];
+
 ?>
 
 <!DOCTYPE html>
