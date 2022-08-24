630cb6b8c363071c776085e34d26338a881d728d
Kushima <myurin0819@gmail.com>
2022-08-24 16:16:11 +0900

    ユーザー画面の編集機能

--- Models/tweets.php
+++ Models/tweets.php
@@ -11,27 +11,30 @@
      */
 function createTweet(array $data)
 {
+    // DB接続
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    // 接続チェック
+    // 接続エラーがある場合->処理停止
     if ($mysqli->connect_errno) {
         echo 'MySQLの接続に失敗しました。: ' . $mysqli->connect_error . "¥n";
         exit;
     }
 
-    // 新規登録のSQLを作成
+    // 新規登録のSQLクエリを作成
     $query = 'INSERT INTO tweets (user_id, body, image_name) VALUES (?, ?, ?)';
+
+    // プリペアドステートメントにクエリを登録
     $statement = $mysqli->prepare($query);
 
-    // 値をセット (i=int, s=string)
+    // プレースホルダにカラム値を紐付け(i=int, s=string)
     $statement->bind_param('iss', $data['user_id'], $data['body'], $data['image_name']);
 
-    // 処理を実行
+    // クエリを実行
     $response = $statement->execute();
     if ($response === false) {
         echo 'エラーメッセージ : ' . $mysqli->error . "¥n";
     }
 
-    // 接続を閉じる
+    // DB接続を開放
     $statement->close();
     $mysqli->close();
 
@@ -43,12 +46,14 @@ function createTweet(array $data)
      * 
      * @param array $user ログインしているユーザー情報
      * @param string $keyword 検索キーワード
+     * @param array $user_ids ユーザーID一覧
      * @return array|false
      */
-function findTweets(array $user, string $keyword = null) 
+function findTweets(array $user, string $keyword = null, array $user_ids = null) 
 {
+    // DB接続
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    // 接続チェック
+    // 接続エラーがある場合->処理停止
     if ($mysqli->connect_errno) {
         echo 'MySQLの接続に失敗しました。: ' . $mysqli->connect_error . "¥n";
         exit;
@@ -75,10 +80,10 @@ function findTweets(array $user, string $keyword = null)
             (SELECT COUNT(*) FROM likes WHERE status = 'active' AND tweet_id = T.id) AS like_count
         FROM
             tweets AS T
-            -- ユーザーテーブルを紐付ける
+            -- ユーザーテーブルをusers.idとtweets.user_idで紐付ける
             JOIN
             users AS U ON U.id = T.user_id AND U.status = 'active'
-            -- いいね！テーブルを紐付ける
+            -- いいね！テーブルをlikes.tweet_idとtweets.idで紐付ける
             LEFT JOIN
             likes AS L ON L.tweet_id = T.id AND L.status = 'active' AND L.user_id = '$login_user_id'
         WHERE
@@ -93,13 +98,24 @@ function findTweets(array $user, string $keyword = null)
         $query .= 'AND CONCAT(U.nickname, U.name, T.body) LIKE "%' . $keyword . '%"';
     }
 
+    // ユーザーIDが指定されている場合
+    if (isset($user_ids)) {
+        foreach ($user_ids as $key =>$user_id) {
+            $user_ids[$key] = $mysqli->real_escape_string($user_id);
+        }
+        $user_ids_csv = '"' . join('","', $user_ids) . '"';
+        // ユーザーID一覧に含まれるユーザーで絞る
+        $query .= ' AND T.user_id IN (' . $user_ids_csv . ')';
+    }
+
     // 新しい順に並び替え
     $query .= ' ORDER BY T.created_at DESC';
     // 表示件数50件
     $query .= ' LIMIT 50';
 
-    // SQL実行
-    if ($result = $mysqli->query($query)) {
+    // クエリ実行
+    $result = $mysqli->query($query);
+    if ($result) {
         // データ配列で受け取る
         $response = $result->fetch_all(MYSQLI_ASSOC);
     } else {
